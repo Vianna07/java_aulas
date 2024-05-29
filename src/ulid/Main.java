5a3d8433e67073cb040180cb67a3a7904ed037c6
Vianna07 <viannagustavo71@gmail.com>
2024-05-29 10:45:50 -0300

    Atualizando a classe ulid

--- src/ulid/Main.java
+++ src/ulid/Main.java
@@ -3,7 +3,7 @@ package ulid;
 public class Main {
 	public static void main(String[] args) {
 		ULID ulid = new ULID();
-		ulid.showTimestamp();
-		System.out.println(ULID.ENCODING_LENGTH);
+		System.out.println(ulid.nextULID());
+		System.out.println(ulid.decodeTimestamp(ulid.nextULID()));
 	}
 }
--- src/ulid/ULID.java
+++ src/ulid/ULID.java
@@ -1,22 +1,60 @@
 package ulid;
 
+import java.util.Random;
+
 public class ULID {
-	public static final char[] ENCODING = "0123456789ABCDEFGHJKMNPQRSTVWXYZ".toCharArray();
-	public static final int ENCODING_LENGTH = ENCODING.length;
+	private static final char[] ENCODING = "0123456789ABCDEFGHJKMNPQRSTVWXYZ".toCharArray();
+	private static final int ENCODING_LENGTH = ENCODING.length;
+	private static final int TIMESTAMP_LENGTH = 10;
+	private static final int RANDOM_PART_LENGTH = 16;
+	private final Random random = new Random();
 	
-	public long timestamp;
+	private long timestamp;
+	private long randomPart;
 	
 	public ULID(long ...timestamp) {
         this.timestamp = (timestamp != null && timestamp.length > 0) ? timestamp[0] : System.currentTimeMillis();
+        this.randomPart = Math.abs(random.nextLong());
 	}
-	
-	public void showTimestamp() {
-		System.out.println(this.timestamp);
-	}
-	
-    public static int[] divMod(int dividend, int divisor) {
-        int quotient = dividend / divisor;
-        int remainder = dividend % divisor;
-        return new int[]{quotient, remainder};
+    
+    private String encode(long value, int limit) {
+    	String encoding = "";
+    	long remainder = 0;
+    	
+    	while (encoding.length() < limit) {
+    		remainder = value % ULID.ENCODING_LENGTH;
+    		value = value / ULID.ENCODING_LENGTH;
+    		encoding = String.valueOf(ULID.ENCODING[(int) remainder]) + encoding;
+       	}
+    	
+    	return encoding;
+    }
+    
+    private String encodeTimestamp() {
+    	return this.encode(this.timestamp, ULID.TIMESTAMP_LENGTH);
+    }
+    
+    private String encodeRandomPart() {
+    	return this.encode(randomPart, ULID.RANDOM_PART_LENGTH);
+    }
+    
+    public String nextULID() {
+    	return this.encodeTimestamp() + this.encodeRandomPart();
+    }
+    
+    private String decode(String code) {
+    	String decoding = "";
+    	
+    	for (char character : code.toCharArray()) {
+//        	System.out.println(character + " - " + ULID.ENCODING.toString().indexOf(character) + " - " + ULID.ENCODING.toString() + " - " + ULID.ENCODING);
+    		decoding = ULID.ENCODING.toString().indexOf(character) + decoding; 
+    	}
+    	
+    	System.out.println(decoding);
+    	return decoding;
+    }
+    
+    public String decodeTimestamp(String ulid) {
+    	return decode(ulid.substring(0, ULID.TIMESTAMP_LENGTH));
     }
 }
